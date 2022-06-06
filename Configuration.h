6c9971ef3b4099f04403490aae1e863a7dfe3415
Sam <50973822+slastrina@users.noreply.github.com>
2022-06-06 21:50:14 +1000

    changed x/y for zprobe offset of bltouch on trianglelab DDE

--- Configuration.h
+++ Configuration.h
@@ -1150,7 +1150,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -40, -10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 38.2, 0, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
